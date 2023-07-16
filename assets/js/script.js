151d61ce6524ad1dc5993c6e0684f78e8f21cc27
VuTran2601 <nguyenvutran2601@gmail.com>
2023-07-16 18:57:32 +0700

    Responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,14 @@
-// Amazon logo
+// Handle scale branch logo
 const amazonLogo = document.getElementById("amazon-logo");
 amazonLogo.addEventListener("mouseover", function () {
     this.style.removeProperty("filter");
     this.style.removeProperty("opacity");
 });
-
 amazonLogo.addEventListener("mouseout", function () {
     this.style.filter = "brightness(75%) saturate(0)";
     this.style.opacity = "0.5";
 });
 
-// Google logo
 const googleLogo = document.getElementById("google-logo");
 googleLogo.addEventListener("mouseover", function () {
     this.style.filter = "";
@@ -21,7 +19,6 @@ googleLogo.addEventListener("mouseout", function () {
     this.style.opacity = "0.5";
 });
 
-// Cocacola logo
 const cocacolaLogo = document.getElementById("cocacola-logo");
 cocacolaLogo.addEventListener("mouseover", function () {
     this.style.filter = "";
@@ -32,7 +29,6 @@ cocacolaLogo.addEventListener("mouseout", function () {
     this.style.opacity = "0.5";
 });
 
-// Airbnb logo
 const airbnbLogo = document.getElementById("airbnb-logo");
 airbnbLogo.addEventListener("mouseover", function () {
     this.style.filter = "";
@@ -43,7 +39,6 @@ airbnbLogo.addEventListener("mouseout", function () {
     this.style.opacity = "0.5";
 });
 
-// Samsung logo
 const samsungLogo = document.getElementById("samsung-logo");
 samsungLogo.addEventListener("mouseover", function () {
     this.style.filter = "";
@@ -53,13 +48,16 @@ samsungLogo.addEventListener("mouseout", function () {
     this.style.filter = "brightness(75%) saturate(0)";
     this.style.opacity = "0.5";
 });
+// END
 
-const buttons = document.querySelectorAll(".furniture-gallery-button > button");
-
-for (let button of buttons) {
+/// Handle switch for customer review
+const customerReviewSliderButtons = document.querySelectorAll(
+    ".customer-reviews > div:nth-child(2) > div:last-child> button"
+);
+for (let button of customerReviewSliderButtons) {
     button.addEventListener("click", function () {
         // Deselect all other buttons
-        buttons.forEach(function (otherButton) {
+        customerReviewSliderButtons.forEach(function (otherButton) {
             if (otherButton !== this) {
                 otherButton.classList.remove("selected");
             }
@@ -69,4 +67,38 @@ for (let button of buttons) {
         this.classList.add("selected");
     });
 }
+const customerReviewSlider = document.querySelector(
+    ".customer-reviews > div:nth-child(2)"
+);
+const review1_Button = document.getElementById("review1");
+review1_Button.addEventListener("click", function () {
+    customerReviewSlider.style.marginLeft = "67%";
+});
+const review2_Button = document.getElementById("review2");
+review2_Button.addEventListener("click", function () {
+    customerReviewSlider.style.marginLeft = "0vw";
+});
+const review3_Button = document.getElementById("review3");
+review3_Button.addEventListener("click", function () {
+    customerReviewSlider.style.marginLeft = "-67%";
+});
+// END
 
+// Handle click furniture gallery button
+const furnitureGalleryButtons = document.querySelectorAll(
+    ".furniture-gallery-button > button"
+);
+for (let button of furnitureGalleryButtons) {
+    button.addEventListener("click", function () {
+        // Deselect all other buttons
+        furnitureGalleryButtons.forEach(function (otherButton) {
+            if (otherButton !== this) {
+                otherButton.classList.remove("selected");
+            }
+        });
+
+        // Select this button
+        this.classList.add("selected");
+    });
+}
+// END
